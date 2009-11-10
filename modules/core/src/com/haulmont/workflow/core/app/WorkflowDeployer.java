8929b6ecea355f987c024ea755bb101a56eed5b7
Konstantin Krivopustov <krivopustov@haulmont.com>
2009-11-10 06:06:57 +0000

    JBoss initialization

---
+++ modules/core/src/com/haulmont/workflow/core/app/WorkflowDeployer.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2009 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+
+ * Author: Konstantin Krivopustov
+ * Created: 09.11.2009 15:48:15
+ *
+ * $Id$
+ */
+package com.haulmont.workflow.core.app;
+
+public class WorkflowDeployer implements WorkflowDeployerMBean {
+}
