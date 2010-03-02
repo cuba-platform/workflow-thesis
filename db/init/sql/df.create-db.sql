77d7f2544ee4a78754ee281bf6e1019f562d556b
Konstantin Krivopustov <krivopustov@haulmont.com>
2010-03-02 12:40:47 +0000

    Docflow moved to separate project

--- db/init/sql/df.create-db.sql
+++
@@ -1,8 +0,0 @@
-create table DF_DOC (
-    CARD_ID varchar(36),
-    NUMBER varchar(50),
-    DATE timestamp,
-    primary key (CARD_ID)
-);
-
-alter table DF_DOC add constraint FK_DF_DOC_CARD foreign key (CARD_ID) references WF_CARD (ID);
