88c8f7e739bcf0ea231546712f4a1a2e4d3debfc
Maxim Chernov <chernov@haulmont.com>
2011-06-30 13:13:22 +0000

    Пользователь теперь может радектировать только свой комментарий в "записях журнала действий"

--- modules/web/src/com/haulmont/workflow/web/ui/base/ResolutionEditor.java
+++ modules/web/src/com/haulmont/workflow/web/ui/base/ResolutionEditor.java
@@ -11,7 +11,11 @@
 package com.haulmont.workflow.web.ui.base;
 
 import com.haulmont.cuba.core.entity.Entity;
+import com.haulmont.cuba.gui.UserSessionClient;
 import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.data.Datasource;
+import com.haulmont.cuba.security.entity.User;
+import com.haulmont.workflow.core.entity.Assignment;
 import java.util.Map;
 
 public class ResolutionEditor extends AbstractEditor {
@@ -28,5 +32,9 @@ public class ResolutionEditor extends AbstractEditor {
     @Override
     public void setItem(Entity item) {
         super.setItem(item);
+        Datasource assignmentDs = getDsContext().get("assignmentDs");
+        Assignment assignment = (Assignment) assignmentDs.getItem();
+        User currentUser = UserSessionClient.getUserSession().getCurrentOrSubstitutedUser();
+        ((TextField) getComponent("comment")).setEditable(currentUser.equals(assignment.getUser()));
     }
 }
