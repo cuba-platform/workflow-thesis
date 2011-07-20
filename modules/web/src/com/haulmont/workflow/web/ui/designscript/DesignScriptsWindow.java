00820757467787001b36a643015565138cc2092e
Konstantin Devyatkin <devyatkin@haulmont.com>
2011-07-20 12:50:43 +0000

    Refs #797 WfDesigner: запретить создание скриптов без имени

--- modules/web/src/com/haulmont/workflow/web/ui/designscript/DesignScriptsWindow.java
+++ modules/web/src/com/haulmont/workflow/web/ui/designscript/DesignScriptsWindow.java
@@ -12,9 +12,13 @@ package com.haulmont.workflow.web.ui.designscript;
 
 import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.data.Datasource;
+import com.haulmont.cuba.gui.data.impl.DsListenerAdapter;
 import com.haulmont.workflow.core.entity.Design;
 import com.haulmont.workflow.core.entity.DesignScript;
+import org.apache.commons.lang.StringUtils;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 
@@ -45,6 +49,16 @@ public class DesignScriptsWindow extends AbstractWindow {
         table.addAction(new NewAction());
         table.addAction(new ModifyAction());
 
+        ds.addListener(new DsListenerAdapter<DesignScript>() {
+            public void itemChanged(Datasource<DesignScript> ds, DesignScript prevItem, DesignScript item) {
+                if (prevItem == null && item != null) {
+                    enableScriptEdit();
+                } else if (item == null) {
+                    disableScriptEdit();
+                }
+            }
+        });
+
         nameField = getComponent("nameField");
         contentField = getComponent("contentField");
         actionsPane = getComponent("actionsPane");
@@ -55,14 +69,22 @@ public class DesignScriptsWindow extends AbstractWindow {
 
     private void enableControls() {
         actionsPane.setVisible(true);
-        nameField.setEditable(true);
-        contentField.setEditable(true);
+        enableScriptEdit();
     }
 
     private void disableControls() {
         actionsPane.setVisible(false);
+        disableScriptEdit();
+    }
+
+    private void disableScriptEdit() {
         nameField.setEditable(false);
         contentField.setEditable(false);
+    }
+
+    private void enableScriptEdit() {
+        nameField.setEditable(true);
+        contentField.setEditable(true);
     }
 
     private class NewAction extends AbstractAction {
@@ -101,6 +123,14 @@ public class DesignScriptsWindow extends AbstractWindow {
         }
 
         public void actionPerform(Component component) {
+            Collection<UUID> designIds = ds.getItemIds();
+            for (UUID id : designIds) {
+                DesignScript designScript = ds.getItem(id);
+                if (StringUtils.trimToNull(designScript.getName()) == null) {
+                    showNotification(getMessage("emptyScriptName"), NotificationType.TRAY);
+                    return;
+                }
+            }
             ds.commit();
             disableControls();
         }
