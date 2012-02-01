05535d85c339155edcfb7709326755cd246d1d3d
Yuriy Artamonov <artamonov@haulmont.com>
2012-02-01 10:50:09 +0000

    FormatSize method moved to FileDownloadHelper

--- modules/web/src/com/haulmont/workflow/web/ui/base/attachments/AttachmentColumnGeneratorHelper.java
+++ modules/web/src/com/haulmont/workflow/web/ui/base/attachments/AttachmentColumnGeneratorHelper.java
@@ -10,17 +10,16 @@
  */
 package com.haulmont.workflow.web.ui.base.attachments;
 
-import com.haulmont.cuba.core.global.MessageProvider;
 import com.haulmont.cuba.gui.components.Table;
 import com.haulmont.cuba.gui.data.Datasource;
 import com.haulmont.cuba.gui.data.impl.DsListenerAdapter;
+import com.haulmont.cuba.web.app.FileDownloadHelper;
 import com.haulmont.cuba.web.gui.components.WebComponentsHelper;
 import com.haulmont.workflow.core.entity.Attachment;
 import com.vaadin.ui.Component;
 
-import java.text.NumberFormat;
-import java.util.UUID;
 import java.util.HashMap;
+import java.util.UUID;
 
 public class AttachmentColumnGeneratorHelper {
 
@@ -47,36 +46,21 @@ public class AttachmentColumnGeneratorHelper {
     {
         final HashMap<UUID, com.vaadin.ui.Component> map = new HashMap<UUID, com.vaadin.ui.Component>();
         ((com.vaadin.ui.Table) WebComponentsHelper.unwrap(attachmentsTable)).addGeneratedColumn(
-                attachmentsTable.getDatasource().getMetaClass().getPropertyEx("file.size"),
+                attachmentsTable.getDatasource().getMetaClass().getPropertyPath("file.size"),
                 new com.vaadin.ui.Table.ColumnGenerator() {
+                    @Override
                     public Component generateCell(com.vaadin.ui.Table table, Object itemId, Object columnId) {
                         UUID uuid = (UUID) itemId;
                         if (map.containsKey(uuid)) {
                             return map.get(uuid);
                         }
                         Attachment attach = (Attachment) attachmentsTable.getDatasource().getItem(uuid);
-                        com.vaadin.ui.Label label = new com.vaadin.ui.Label(formatSize(attach.getFile().getSize(), 0));
+                        String formattedSize = FileDownloadHelper.formatFileSize(attach.getFile().getSize());
+                        com.vaadin.ui.Label label = new com.vaadin.ui.Label(formattedSize);
                         label.setWidth("-1px");
                         map.put(uuid, label);
                         return label;
                     }
                 });
-    }
-
-    private static String formatSize(long longSize, int decimalPos) {
-        NumberFormat fmt = NumberFormat.getNumberInstance();
-        if (decimalPos >= 0) {
-            fmt.setMaximumFractionDigits(decimalPos);
-        }
-        final double size = longSize;
-        double val = size / (1024 * 1024);
-        if (val > 1) {
-            return fmt.format(val).concat(" " + MessageProvider.getMessage(AttachmentColumnGeneratorHelper.class, "fmtMb"));
-        }
-        val = size / 1024;
-        if (val > 10) {
-            return fmt.format(val).concat(" " + MessageProvider.getMessage(AttachmentColumnGeneratorHelper.class, "fmtKb"));
-        }
-        return fmt.format(size).concat(" " + MessageProvider.getMessage(AttachmentColumnGeneratorHelper.class, "fmtB"));
     }
 }
