d42fe80024a2c90cdc486951f6c6d9b0ec334b81
Yuriy Artamonov <artamonov@haulmont.com>
2012-10-29 13:26:49 +0000

    Refs #1606 Workflow: TimerManager - unnecessary login/logout on every scheduler tick

--- modules/core/src/com/haulmont/workflow/core/app/TimerManager.java
+++ modules/core/src/com/haulmont/workflow/core/app/TimerManager.java
@@ -2,11 +2,6 @@
  * Copyright (c) 2009 Haulmont Technology Ltd. All Rights Reserved.
  * Haulmont Technology proprietary and confidential.
  * Use is subject to license terms.
-
- * Author: Konstantin Krivopustov
- * Created: 26.01.2010 10:34:07
- *
- * $Id$
  */
 package com.haulmont.workflow.core.app;
 
@@ -15,10 +10,9 @@ import com.haulmont.cuba.core.*;
 import com.haulmont.cuba.core.app.ClusterManagerAPI;
 import com.haulmont.cuba.core.app.ManagementBean;
 import com.haulmont.cuba.core.global.EntityLoadInfo;
-import com.haulmont.cuba.core.global.ScriptingProvider;
-import com.haulmont.cuba.core.global.TimeProvider;
+import com.haulmont.cuba.core.global.Scripting;
+import com.haulmont.cuba.core.global.TimeSource;
 import com.haulmont.cuba.core.sys.AppContext;
-import com.haulmont.cuba.core.sys.SecurityContext;
 import com.haulmont.cuba.security.entity.User;
 import com.haulmont.cuba.security.global.LoginException;
 import com.haulmont.workflow.core.entity.Assignment;
@@ -41,7 +35,12 @@ import java.util.List;
 import java.util.Map;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * @author krivopustov
+ * @version $Id$
+ */
 @ManagedBean(TimerManagerAPI.NAME)
 public class TimerManager extends ManagementBean implements TimerManagerAPI, TimerManagerMBean {
 
@@ -53,12 +52,22 @@ public class TimerManager extends ManagementBean implements TimerManagerAPI, Tim
     @Inject
     private WorkCalendarAPI workCalendarAPI;
 
+    @Inject
+    private Persistence persistence;
+
+    @Inject
+    private TimeSource timeSource;
+
+    @Inject
+    private Scripting scripting;
+
+    @Override
     public void addTimer(Card card, @Nullable ActivityExecution execution, Date dueDate,
                          Class<? extends TimerAction> taskClass, Map<String, String> taskParams) {
         checkArgument(card != null, "card is null");
         checkArgument(dueDate != null, "dueDate is null");
 
-        EntityManager em = PersistenceProvider.getEntityManager();
+        EntityManager em = persistence.getEntityManager();
 
         TimerEntity timer = new TimerEntity();
         timer.setCard(card);
@@ -77,25 +86,30 @@ public class TimerManager extends ManagementBean implements TimerManagerAPI, Tim
         em.persist(timer);
     }
 
+    @Override
     public void removeTimers(ActivityExecution execution) {
-        checkArgument(execution != null, "execution is null");
+        checkNotNull(execution, "execution is null");
 
-        EntityManager em = PersistenceProvider.getEntityManager();
+        EntityManager em = persistence.getEntityManager();
         Query q = em.createQuery("delete from wf$Timer t where t.jbpmExecutionId = ?1 and t.activity = ?2");
         q.setParameter(1, execution.getId());
         q.setParameter(2, execution.getActivityName());
         q.executeUpdate();
     }
 
+    @Override
     public void removeTimers(ActivityExecution execution, Assignment assignment) {
-        checkArgument(execution != null, "execution is null");
-        checkArgument(assignment != null, "assignment is null");
+        checkNotNull(execution, "execution is null");
+        checkNotNull(assignment, "assignment is null");
 
-        EntityManager em = PersistenceProvider.getEntityManager();
-        Query q = em.createQuery("select t from wf$Timer t where t.jbpmExecutionId = ?1 and t.activity = ?2");
+        EntityManager em = persistence.getEntityManager();
+        TypedQuery<TimerEntity> q = em.createQuery(
+                "select t from wf$Timer t where t.jbpmExecutionId = ?1 and t.activity = ?2",
+                TimerEntity.class
+        );
         q.setParameter(1, execution.getId());
         q.setParameter(2, execution.getActivityName());
-        List<TimerEntity> timers =  q.getResultList();
+        List<TimerEntity> timers = q.getResultList();
 
         for (TimerEntity timerEntity : timers) {
             Map<String, String> params = getTimerActionParams(timerEntity.getActionParams());
@@ -112,64 +126,78 @@ public class TimerManager extends ManagementBean implements TimerManagerAPI, Tim
         }
     }
 
+    @Override
     public void processTimers() {
         if (!AppContext.isStarted() || !clusterManager.isMaster())
             return;
 
         log.debug("Processing timers");
         try {
-            login();
+            loginOnce();
 
-            List<TimerEntity> timers;
-
-            Date currentTime = TimeProvider.currentTimestamp();
+            Date currentTime = timeSource.currentTimestamp();
             if (!workCalendarAPI.isDateWorkDay(currentTime))
                 return;
 
-            Transaction tx = Locator.createTransaction();
-            try {
-                EntityManager em = PersistenceProvider.getEntityManager();
-                Query q = em.createQuery("select t from wf$Timer t where t.dueDate <= ?1 order by t.dueDate desc");
-                q.setParameter(1, currentTime);
-                timers = q.getResultList();
-                tx.commit();
-            } finally {
-                tx.end();
-            }
+            List<TimerEntity> timers = loadTimers(currentTime);
 
             for (TimerEntity timer : timers) {
-                try {
-                    Class<? extends TimerAction> taskClass = ScriptingProvider.loadClass(timer.getActionClass());
-                    TimerAction action = taskClass.newInstance();
-
-                    tx = Locator.createTransaction();
-                    try {
-                        EntityManager em = PersistenceProvider.getEntityManager();
-                        TimerEntity t = em.find(TimerEntity.class, timer.getId());
-
-                        TimerActionContext context = new TimerActionContext(t.getCard(), t.getJbpmExecutionId(),
-                                t.getActivity(), t.getDueDate(), getTimerActionParams(t.getActionParams()));
-                        action.execute(context);
-
-                        em.remove(t);
-
-                        tx.commit();
-                    } finally {
-                        tx.end();
-                    }
-                } catch (Throwable e) {
-                    log.error("Error firing timer " + timer, e);
-                }
+                fireTimer(timer);
             }
         } catch (LoginException e) {
             throw new RuntimeException(e);
         } finally {
-            logout();
+            clearSecurityContext();
         }
     }
 
+    private void fireTimer(TimerEntity timer) {
+        try {
+            Class<? extends TimerAction> taskClass = scripting.loadClass(timer.getActionClass());
+            TimerAction action = taskClass.newInstance();
+
+            Transaction tx = persistence.createTransaction();
+            try {
+                EntityManager em = persistence.getEntityManager();
+                TimerEntity t = em.find(TimerEntity.class, timer.getId());
+
+                TimerActionContext context = new TimerActionContext(t.getCard(), t.getJbpmExecutionId(),
+                        t.getActivity(), t.getDueDate(), getTimerActionParams(t.getActionParams()));
+                action.execute(context);
+
+                em.remove(t);
+
+                tx.commit();
+            } finally {
+                tx.end();
+            }
+        } catch (Throwable e) {
+            log.error("Error firing timer " + timer, e);
+        }
+    }
+
+    private List<TimerEntity> loadTimers(Date currentTime) {
+        List<TimerEntity> timers;
+
+        Transaction tx = persistence.createTransaction();
+        try {
+            EntityManager em = persistence.getEntityManager();
+            TypedQuery<TimerEntity> q = em.createQuery(
+                    "select t from wf$Timer t where t.dueDate <= ?1 order by t.dueDate desc",
+                    TimerEntity.class
+            );
+
+            q.setParameter(1, currentTime);
+            timers = q.getResultList();
+            tx.commit();
+        } finally {
+            tx.end();
+        }
+        return timers;
+    }
+
     private Map<String, String> getTimerActionParams(String actionParams) {
-        Map<String, String> map = new HashMap<String, String>();
+        Map<String, String> map = new HashMap<>();
 
         Document doc = Dom4j.readDocument(actionParams);
         Dom4j.loadMap(doc.getRootElement().element("map"), map);
