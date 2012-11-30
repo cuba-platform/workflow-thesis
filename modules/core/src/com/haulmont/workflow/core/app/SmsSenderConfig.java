b747dbf6ece1ca0b5546133a2347076f285d323d
Konstantin Krivopustov <krivopustov@haulmont.com>
2012-11-30 07:47:03 +0000

    Refs #1705 Only explicit property names in config interfaces

--- modules/core/src/com/haulmont/workflow/core/app/SmsSenderConfig.java
+++ modules/core/src/com/haulmont/workflow/core/app/SmsSenderConfig.java
@@ -7,7 +7,7 @@
 package com.haulmont.workflow.core.app;
 
 import com.haulmont.cuba.core.config.Config;
-import com.haulmont.cuba.core.config.Prefix;
+import com.haulmont.cuba.core.config.Property;
 import com.haulmont.cuba.core.config.Source;
 import com.haulmont.cuba.core.config.SourceType;
 import com.haulmont.cuba.core.config.defaults.Default;
@@ -15,62 +15,59 @@ import com.haulmont.cuba.core.config.defaults.DefaultBoolean;
 import com.haulmont.cuba.core.config.defaults.DefaultInt;
 
 /**
- * <p>$Id$</p>
- *
  * @author novikov
+ * @version $Id$
  */
-@Prefix("workflow.sms.")
 @Source(type = SourceType.DATABASE)
 public interface SmsSenderConfig extends Config {
+
     /**
      * Used in server startup.
      * Sending smses will be started after delayCallCount cron ticks (used to not overload server in startup)
      */
+    @Property("workflow.sms.delayCallCount")
     @Default("2")
     int getDelayCallCount();
 
     /**
      * MaxResults query limit for load messages from DB in one tick
      */
+    @Property("workflow.sms.messageQueueCapacity")
     @Default("100")
     int getMessageQueueCapacity();
 
     /**
-     * @return Quantity of sending attempts
+     * @return Number of sending attempts
      */
+    @Property("workflow.sms.defaultSendingAttemptsCount")
     @DefaultInt(10)
     int getDefaultSendingAttemptsCount();
 
     /**
      * Use SmsMenager
-     *
-     * @return
      */
+    @Property("workflow.sms.useSmsSending")
     @DefaultBoolean(false)
     boolean getUseSmsSending();
-
     void setUseSmsSending(boolean value);
 
     /**
-     * Use SmsProvider by default
-     *
-     * @return
+     * Use default SmsProvider
      */
+    @Property("workflow.sms.useDefaultSmsSending")
     @DefaultBoolean(false)
     boolean getUseDefaultSmsSending();
-
     void setUseDefaultSmsSending(boolean value);
 
     /**
      * Class name SmsProvider
-     *
-     * @return
      */
+    @Property("workflow.sms.smsProviderClassName")
     @Default("com.haulmont.workflow.core.app.DefaultSmsProvider")
     String getSmsProviderClassName();
-
     void setSmsProviderClassName(String value);
 
+    @Property("workflow.sms.maxSendingTimeSec")
     @DefaultInt(86400)
     int getMaxSendingTimeSec();
 }
--- modules/global/src/com/haulmont/workflow/core/global/WfConfig.java
+++ modules/global/src/com/haulmont/workflow/core/global/WfConfig.java
@@ -11,25 +11,28 @@
 package com.haulmont.workflow.core.global;
 
 import com.haulmont.cuba.core.config.Config;
-import com.haulmont.cuba.core.config.Prefix;
+import com.haulmont.cuba.core.config.Property;
 import com.haulmont.cuba.core.config.Source;
 import com.haulmont.cuba.core.config.SourceType;
 import com.haulmont.cuba.core.config.defaults.Default;
 import com.haulmont.cuba.core.config.defaults.DefaultBoolean;
 
-@Prefix("workflow.")
 @Source(type = SourceType.APP)
 public interface WfConfig extends Config {
 
+    @Property("workflow.designerUrl")
     @Default("wfdesigner/workflow/main.ftl")
     String getDesignerUrl();
 
+    @Property("workflow.defaultAttachmentType")
     @Default("AttachmentType.attachment")
     String getDefaultAttachmentType();
     
+    @Property("workflow.notificationTemplatePath")
     @Default("/com/haulmont/workflow/core/NotificationMatrixTemplate.xls")
     String getNotificationTemplatePath();
 
+    @Property("workflow.oneAttachmentUploaderEnabled")
     @DefaultBoolean(false)
     boolean getOneAttachmentUploaderEnabled();
     void setOneAttachmentUploaderEnabled(boolean value);
