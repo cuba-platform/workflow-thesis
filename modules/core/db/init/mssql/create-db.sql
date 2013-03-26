b9617adbef00e255753bd549ea0df5c1864700af
Vladimir Shakhov <shakhov@haulmont.com>
2013-03-26 08:58:39 +0000

    Добавление нового поля recognizedFileDescriptor(ссылка на распознанный файл) в сущность Attachment #PL-1976

--- modules/core/db/init/mssql/create-db.sql
+++ modules/core/db/init/mssql/create-db.sql
@@ -290,6 +290,7 @@ create table WF_ATTACHMENT (
     CARD_ID uniqueidentifier,
     ASSIGNMENT_ID uniqueidentifier,
     VERSION_OF_ID uniqueidentifier,
+    RECOGNIZED_FILE_DESCRIPTOR_ID uniqueidentifier,
     VERSION_NUM integer,
     primary key nonclustered (ID)
 )^
@@ -303,6 +304,8 @@ alter table WF_ATTACHMENT add constraint FK_WF_ATTACHMENT_ASSIGNMENT foreign key
 alter table WF_ATTACHMENT add constraint FK_WF_ATTACHMENT_TYPE foreign key (TYPE_ID) references WF_ATTACHMENTTYPE (ID)^
 
 alter table WF_ATTACHMENT add constraint FK_WF_ATTACHMENT_ATTACHMENT foreign key (VERSION_OF_ID) references WF_ATTACHMENT (ID)^
+
+alter table WF_ATTACHMENT add constraint FK_WF_RECOGNIZED_FILE foreign key (RECOGNIZED_FILE_DESCRIPTOR_ID) references SYS_FILE (ID)^
 
 create clustered index IDX_WF_ATTACHMENT_CARD on WF_ATTACHMENT (CARD_ID)^
 
