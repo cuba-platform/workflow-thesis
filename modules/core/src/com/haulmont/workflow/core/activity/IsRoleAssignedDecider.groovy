16c326f0c84dd8e3b7ff6dd130296a5d9ac7157e
Maxim Gorbunkov <gorbunkov@haulmont.com>
2013-05-14 06:34:42 +0000

    Merged from release_3_1 Модуль "Роль назначена" должен работать только по ролям текущего активного процесса [from revision 9977] #PL-1753

--- modules/core/src/com/haulmont/workflow/core/activity/IsRoleAssignedDecider.groovy
+++ modules/core/src/com/haulmont/workflow/core/activity/IsRoleAssignedDecider.groovy
@@ -10,46 +10,47 @@
 
 
 
-package com.haulmont.workflow.core.activity;
+package com.haulmont.workflow.core.activity
 
-import com.haulmont.workflow.core.entity.Card;
-import com.haulmont.workflow.core.entity.CardRole;
-import org.jbpm.api.activity.ActivityExecution;
-import org.jbpm.api.activity.ExternalActivityBehaviour;
+import com.haulmont.workflow.core.entity.Card
+import com.haulmont.workflow.core.entity.CardRole
+import org.jbpm.api.activity.ActivityExecution
+import org.jbpm.api.activity.ExternalActivityBehaviour
 
 public class IsRoleAssignedDecider implements ExternalActivityBehaviour {
 
-  private String role;
+    private String role;
 
-  public void setRole(String role) {
-    this.role = role;
-  }
-
-  public String getRole() {
-    return role;
-  }
-
-  public void signal(ActivityExecution execution, String signalName, Map<String, ?> parameters) throws Exception {
-    execution.take(signalName);
-  }
-
-  public void execute(ActivityExecution execution) throws Exception {
-    Card card = ActivityHelper.findCard(execution);
-    card.getRoles().get(0).getCode();
-    CardRole cardRole = findRole(card);
-    if ((cardRole != null) && (cardRole.getUser() != null)) {
-      execution.take("yes");
-    } else {
-      execution.take("no");
+    public void setRole(String role) {
+        this.role = role;
     }
-  }
 
-  private CardRole findRole(Card card) {
-    for (CardRole cardRole: card.getRoles()) {
-      if (cardRole.getCode().equals(role))
-        return cardRole;
+    public String getRole() {
+        return role;
     }
-    return null;
-  }
+
+    public void signal(ActivityExecution execution, String signalName, Map<String, ?> parameters) throws Exception {
+        execution.take(signalName);
+    }
+
+    public void execute(ActivityExecution execution) throws Exception {
+        Card card = ActivityHelper.findCard(execution);
+        card.getRoles().get(0).getCode();
+        CardRole cardRole = findRole(card);
+        if ((cardRole != null) && (cardRole.getUser() != null)) {
+            execution.take("yes");
+        } else {
+            execution.take("no");
+        }
+    }
+
+    private CardRole findRole(Card card) {
+        for (CardRole cardRole : card.getRoles()) {
+            if (cardRole.getCode().equals(role) && cardRole.procRole != null && cardRole.procRole.proc == card.proc) {
+                return cardRole;
+            }
+        }
+        return null;
+    }
 
 }
