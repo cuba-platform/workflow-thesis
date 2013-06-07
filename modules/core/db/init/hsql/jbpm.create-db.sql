8923a55d7a833cd6eb23e6e701953e6b17e677fa
Konstantin Krivopustov <krivopustov@haulmont.com>
2013-06-07 07:35:51 +0000

    HSQL support in all base projects. #PL-2198

--- modules/core/db/init/hsql/jbpm.create-db.sql
+++ modules/core/db/init/hsql/jbpm.create-db.sql
@@ -5,7 +5,7 @@
         TIMESTAMP_ bigint,
         STATE_ varchar(255),
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_DEPLOYPROP (
         DBID_ bigint not null,
@@ -15,7 +15,7 @@
         STRINGVAL_ varchar(255),
         LONGVAL_ bigint,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_EXECUTION (
         DBID_ bigint not null,
@@ -38,7 +38,7 @@
         PARENT_IDX_ integer,
         primary key (DBID_),
         unique (ID_)
-    );
+    )^
 
     create table JBPM4_HIST_ACTINST (
         DBID_ bigint not null,
@@ -55,7 +55,7 @@
         NEXTIDX_ integer,
         HTASK_ bigint,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_HIST_DETAIL (
         DBID_ bigint not null,
@@ -81,7 +81,7 @@
         PARENT_ bigint,
         PARENT_IDX_ integer,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_HIST_PROCINST (
         DBID_ bigint not null,
@@ -96,7 +96,7 @@
         ENDACTIVITY_ varchar(255),
         NEXTIDX_ integer,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_HIST_TASK (
         DBID_ bigint not null,
@@ -112,7 +112,7 @@
         NEXTIDX_ integer,
         SUPERTASK_ bigint,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_HIST_VAR (
         DBID_ bigint not null,
@@ -124,7 +124,7 @@
         HPROCI_ bigint,
         HTASK_ bigint,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_ID_GROUP (
         DBID_ bigint not null,
@@ -134,7 +134,7 @@
         TYPE_ varchar(255),
         PARENT_ bigint,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_ID_MEMBERSHIP (
         DBID_ bigint not null,
@@ -143,7 +143,7 @@
         GROUP_ bigint,
         NAME_ varchar(255),
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_ID_USER (
         DBID_ bigint not null,
@@ -154,7 +154,7 @@
         FAMILYNAME_ varchar(255),
         BUSINESSEMAIL_ varchar(255),
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_JOB (
         DBID_ bigint not null,
@@ -174,7 +174,7 @@
         EVENT_ varchar(255),
         REPEAT_ varchar(255),
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_LOB (
         DBID_ bigint not null,
@@ -183,7 +183,7 @@
         DEPLOYMENT_ bigint,
         NAME_ longvarchar,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_PARTICIPATION (
         DBID_ bigint not null,
@@ -194,14 +194,14 @@
         TASK_ bigint,
         SWIMLANE_ bigint,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_PROPERTY (
         KEY_ varchar(255) not null,
         VERSION_ integer not null,
         VALUE_ varchar(255),
         primary key (KEY_)
-    );
+    )^
 
     create table JBPM4_SWIMLANE (
         DBID_ bigint not null,
@@ -210,7 +210,7 @@
         ASSIGNEE_ varchar(255),
         EXECUTION_ bigint,
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_TASK (
         DBID_ bigint not null,
@@ -236,7 +236,7 @@
         SWIMLANE_ bigint,
         TASKDEFNAME_ varchar(255),
         primary key (DBID_)
-    );
+    )^
 
     create table JBPM4_VARIABLE (
         DBID_ bigint not null,
@@ -256,206 +256,206 @@
         TEXT_VALUE_ longvarchar,
         EXESYS_ bigint,
         primary key (DBID_)
-    );
+    )^
 
-    create index IDX_DEPLPROP_DEPL on JBPM4_DEPLOYPROP (DEPLOYMENT_);
+    create index IDX_DEPLPROP_DEPL on JBPM4_DEPLOYPROP (DEPLOYMENT_)^
 
     alter table JBPM4_DEPLOYPROP 
         add constraint FK_DEPLPROP_DEPL 
         foreign key (DEPLOYMENT_) 
-        references JBPM4_DEPLOYMENT;
+        references JBPM4_DEPLOYMENT^
 
-    create index IDX_EXEC_SUPEREXEC on JBPM4_EXECUTION (SUPEREXEC_);
+    create index IDX_EXEC_SUPEREXEC on JBPM4_EXECUTION (SUPEREXEC_)^
 
-    create index IDX_EXEC_INSTANCE on JBPM4_EXECUTION (INSTANCE_);
+    create index IDX_EXEC_INSTANCE on JBPM4_EXECUTION (INSTANCE_)^
 
-    create index IDX_EXEC_SUBPI on JBPM4_EXECUTION (SUBPROCINST_);
+    create index IDX_EXEC_SUBPI on JBPM4_EXECUTION (SUBPROCINST_)^
 
-    create index IDX_EXEC_PARENT on JBPM4_EXECUTION (PARENT_);
+    create index IDX_EXEC_PARENT on JBPM4_EXECUTION (PARENT_)^
 
     alter table JBPM4_EXECUTION 
         add constraint FK_EXEC_PARENT 
         foreign key (PARENT_) 
-        references JBPM4_EXECUTION;
+        references JBPM4_EXECUTION^
 
     alter table JBPM4_EXECUTION 
         add constraint FK_EXEC_SUBPI 
         foreign key (SUBPROCINST_) 
-        references JBPM4_EXECUTION;
+        references JBPM4_EXECUTION^
 
     alter table JBPM4_EXECUTION 
         add constraint FK_EXEC_INSTANCE 
         foreign key (INSTANCE_) 
-        references JBPM4_EXECUTION;
+        references JBPM4_EXECUTION^
 
     alter table JBPM4_EXECUTION 
         add constraint FK_EXEC_SUPEREXEC 
         foreign key (SUPEREXEC_) 
-        references JBPM4_EXECUTION;
+        references JBPM4_EXECUTION^
 
-    create index IDX_HACTI_HPROCI on JBPM4_HIST_ACTINST (HPROCI_);
+    create index IDX_HACTI_HPROCI on JBPM4_HIST_ACTINST (HPROCI_)^
 
-    create index IDX_HTI_HTASK on JBPM4_HIST_ACTINST (HTASK_);
+    create index IDX_HTI_HTASK on JBPM4_HIST_ACTINST (HTASK_)^
 
     alter table JBPM4_HIST_ACTINST 
         add constraint FK_HACTI_HPROCI 
         foreign key (HPROCI_) 
-        references JBPM4_HIST_PROCINST;
+        references JBPM4_HIST_PROCINST^
 
     alter table JBPM4_HIST_ACTINST 
         add constraint FK_HTI_HTASK 
         foreign key (HTASK_) 
-        references JBPM4_HIST_TASK;
+        references JBPM4_HIST_TASK^
 
-    create index IDX_HDET_HACTI on JBPM4_HIST_DETAIL (HACTI_);
+    create index IDX_HDET_HACTI on JBPM4_HIST_DETAIL (HACTI_)^
 
-    create index IDX_HDET_HPROCI on JBPM4_HIST_DETAIL (HPROCI_);
+    create index IDX_HDET_HPROCI on JBPM4_HIST_DETAIL (HPROCI_)^
 
-    create index IDX_HDET_HVAR on JBPM4_HIST_DETAIL (HVAR_);
+    create index IDX_HDET_HVAR on JBPM4_HIST_DETAIL (HVAR_)^
 
-    create index IDX_HDET_HTASK on JBPM4_HIST_DETAIL (HTASK_);
+    create index IDX_HDET_HTASK on JBPM4_HIST_DETAIL (HTASK_)^
 
     alter table JBPM4_HIST_DETAIL 
         add constraint FK_HDETAIL_HPROCI 
         foreign key (HPROCI_) 
-        references JBPM4_HIST_PROCINST;
+        references JBPM4_HIST_PROCINST^
 
     alter table JBPM4_HIST_DETAIL 
         add constraint FK_HDETAIL_HACTI 
         foreign key (HACTI_) 
-        references JBPM4_HIST_ACTINST;
+        references JBPM4_HIST_ACTINST^
 
     alter table JBPM4_HIST_DETAIL 
         add constraint FK_HDETAIL_HTASK 
         foreign key (HTASK_) 
-        references JBPM4_HIST_TASK;
+        references JBPM4_HIST_TASK^
 
     alter table JBPM4_HIST_DETAIL 
         add constraint FK_HDETAIL_HVAR 
         foreign key (HVAR_) 
-        references JBPM4_HIST_VAR;
+        references JBPM4_HIST_VAR^
 
-    create index IDX_HSUPERT_SUB on JBPM4_HIST_TASK (SUPERTASK_);
+    create index IDX_HSUPERT_SUB on JBPM4_HIST_TASK (SUPERTASK_)^
 
     alter table JBPM4_HIST_TASK 
         add constraint FK_HSUPERT_SUB 
         foreign key (SUPERTASK_) 
-        references JBPM4_HIST_TASK;
+        references JBPM4_HIST_TASK^
 
-    create index IDX_HVAR_HPROCI on JBPM4_HIST_VAR (HPROCI_);
+    create index IDX_HVAR_HPROCI on JBPM4_HIST_VAR (HPROCI_)^
 
-    create index IDX_HVAR_HTASK on JBPM4_HIST_VAR (HTASK_);
+    create index IDX_HVAR_HTASK on JBPM4_HIST_VAR (HTASK_)^
 
     alter table JBPM4_HIST_VAR 
         add constraint FK_HVAR_HPROCI 
         foreign key (HPROCI_) 
-        references JBPM4_HIST_PROCINST;
+        references JBPM4_HIST_PROCINST^
 
     alter table JBPM4_HIST_VAR 
         add constraint FK_HVAR_HTASK 
         foreign key (HTASK_) 
-        references JBPM4_HIST_TASK;
+        references JBPM4_HIST_TASK^
 
-    create index IDX_GROUP_PARENT on JBPM4_ID_GROUP (PARENT_);
+    create index IDX_GROUP_PARENT on JBPM4_ID_GROUP (PARENT_)^
 
     alter table JBPM4_ID_GROUP 
         add constraint FK_GROUP_PARENT 
         foreign key (PARENT_) 
-        references JBPM4_ID_GROUP;
+        references JBPM4_ID_GROUP^
 
-    create index IDX_MEM_USER on JBPM4_ID_MEMBERSHIP (USER_);
+    create index IDX_MEM_USER on JBPM4_ID_MEMBERSHIP (USER_)^
 
-    create index IDX_MEM_GROUP on JBPM4_ID_MEMBERSHIP (GROUP_);
+    create index IDX_MEM_GROUP on JBPM4_ID_MEMBERSHIP (GROUP_)^
 
     alter table JBPM4_ID_MEMBERSHIP 
         add constraint FK_MEM_GROUP 
         foreign key (GROUP_) 
-        references JBPM4_ID_GROUP;
+        references JBPM4_ID_GROUP^
 
     alter table JBPM4_ID_MEMBERSHIP 
         add constraint FK_MEM_USER 
         foreign key (USER_) 
-        references JBPM4_ID_USER;
+        references JBPM4_ID_USER^
 
-    create index IDX_JOBRETRIES on JBPM4_JOB (RETRIES_);
+    create index IDX_JOBRETRIES on JBPM4_JOB (RETRIES_)^
 
-    create index IDX_JOB_CFG on JBPM4_JOB (CFG_);
+    create index IDX_JOB_CFG on JBPM4_JOB (CFG_)^
 
-    create index IDX_JOB_PRINST on JBPM4_JOB (PROCESSINSTANCE_);
+    create index IDX_JOB_PRINST on JBPM4_JOB (PROCESSINSTANCE_)^
 
-    create index IDX_JOB_EXE on JBPM4_JOB (EXECUTION_);
+    create index IDX_JOB_EXE on JBPM4_JOB (EXECUTION_)^
 
-    create index IDX_JOBLOCKEXP on JBPM4_JOB (LOCKEXPTIME_);
+    create index IDX_JOBLOCKEXP on JBPM4_JOB (LOCKEXPTIME_)^
 
-    create index IDX_JOBDUEDATE on JBPM4_JOB (DUEDATE_);
+    create index IDX_JOBDUEDATE on JBPM4_JOB (DUEDATE_)^
 
     alter table JBPM4_JOB 
         add constraint FK_JOB_CFG 
         foreign key (CFG_) 
-        references JBPM4_LOB;
+        references JBPM4_LOB^
 
-    create index IDX_LOB_DEPLOYMENT on JBPM4_LOB (DEPLOYMENT_);
+    create index IDX_LOB_DEPLOYMENT on JBPM4_LOB (DEPLOYMENT_)^
 
     alter table JBPM4_LOB 
         add constraint FK_LOB_DEPLOYMENT 
         foreign key (DEPLOYMENT_) 
-        references JBPM4_DEPLOYMENT;
+        references JBPM4_DEPLOYMENT^
 
-    create index IDX_PART_TASK on JBPM4_PARTICIPATION (TASK_);
+    create index IDX_PART_TASK on JBPM4_PARTICIPATION (TASK_)^
 
     alter table JBPM4_PARTICIPATION 
         add constraint FK_PART_SWIMLANE 
         foreign key (SWIMLANE_) 
-        references JBPM4_SWIMLANE;
+        references JBPM4_SWIMLANE^
 
     alter table JBPM4_PARTICIPATION 
         add constraint FK_PART_TASK 
         foreign key (TASK_) 
-        references JBPM4_TASK;
+        references JBPM4_TASK^
 
-    create index IDX_SWIMLANE_EXEC on JBPM4_SWIMLANE (EXECUTION_);
+    create index IDX_SWIMLANE_EXEC on JBPM4_SWIMLANE (EXECUTION_)^
 
     alter table JBPM4_SWIMLANE 
         add constraint FK_SWIMLANE_EXEC 
         foreign key (EXECUTION_) 
-        references JBPM4_EXECUTION;
+        references JBPM4_EXECUTION^
 
-    create index IDX_TASK_SUPERTASK on JBPM4_TASK (SUPERTASK_);
+    create index IDX_TASK_SUPERTASK on JBPM4_TASK (SUPERTASK_)^
 
     alter table JBPM4_TASK 
         add constraint FK_TASK_SWIML 
         foreign key (SWIMLANE_) 
-        references JBPM4_SWIMLANE;
+        references JBPM4_SWIMLANE^
 
     alter table JBPM4_TASK 
         add constraint FK_TASK_SUPERTASK 
         foreign key (SUPERTASK_) 
-        references JBPM4_TASK;
+        references JBPM4_TASK^
 
-    create index IDX_VAR_EXESYS on JBPM4_VARIABLE (EXESYS_);
+    create index IDX_VAR_EXESYS on JBPM4_VARIABLE (EXESYS_)^
 
-    create index IDX_VAR_TASK on JBPM4_VARIABLE (TASK_);
+    create index IDX_VAR_TASK on JBPM4_VARIABLE (TASK_)^
 
-    create index IDX_VAR_EXECUTION on JBPM4_VARIABLE (EXECUTION_);
+    create index IDX_VAR_EXECUTION on JBPM4_VARIABLE (EXECUTION_)^
 
-    create index IDX_VAR_LOB on JBPM4_VARIABLE (LOB_);
+    create index IDX_VAR_LOB on JBPM4_VARIABLE (LOB_)^
 
     alter table JBPM4_VARIABLE 
         add constraint FK_VAR_LOB 
         foreign key (LOB_) 
-        references JBPM4_LOB;
+        references JBPM4_LOB^
 
     alter table JBPM4_VARIABLE 
         add constraint FK_VAR_EXECUTION 
         foreign key (EXECUTION_) 
-        references JBPM4_EXECUTION;
+        references JBPM4_EXECUTION^
 
     alter table JBPM4_VARIABLE 
         add constraint FK_VAR_EXESYS 
         foreign key (EXESYS_) 
-        references JBPM4_EXECUTION;
+        references JBPM4_EXECUTION^
 
     alter table JBPM4_VARIABLE 
         add constraint FK_VAR_TASK 
         foreign key (TASK_) 
-        references JBPM4_TASK;
+        references JBPM4_TASK^
