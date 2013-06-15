3621d10db126b166a7bca1fe4691f213e09ce971
Sergey Saiyan <saiyan@haulmont.com>
2013-06-15 09:43:21 +0000

    fix cancelled process problems trunk #PL-2173 Fixed

--- modules/web/src/com/haulmont/workflow/web/ui/base/action/ProcessAction.java
+++ modules/web/src/com/haulmont/workflow/web/ui/base/action/ProcessAction.java
@@ -12,10 +12,10 @@ package com.haulmont.workflow.web.ui.base.action;
 
 import com.haulmont.cuba.core.app.DataService;
 import com.haulmont.cuba.core.global.*;
-import com.haulmont.cuba.core.sys.AppContext;
 import com.haulmont.cuba.gui.AppConfig;
 import com.haulmont.cuba.gui.ComponentsHelper;
 import com.haulmont.cuba.gui.ServiceLocator;
+import com.haulmont.cuba.gui.WindowManagerProvider;
 import com.haulmont.cuba.gui.components.*;
 import com.haulmont.cuba.gui.data.DsContext;
 import com.haulmont.cuba.web.App;
@@ -23,6 +23,7 @@ import com.haulmont.cuba.web.gui.WebWindow;
 import com.haulmont.workflow.core.app.WfService;
 import com.haulmont.workflow.core.entity.Assignment;
 import com.haulmont.workflow.core.entity.Card;
+import com.haulmont.workflow.core.entity.CardProc;
 import com.haulmont.workflow.core.global.AssignmentInfo;
 import com.haulmont.workflow.core.global.WfConstants;
 
@@ -39,6 +40,7 @@ public class ProcessAction extends AbstractAction {
 
     protected Messages messages = AppBeans.get(Messages.NAME);
     protected WfService wfService = AppBeans.get(WfService.NAME);
+    protected DataService dataService = AppBeans.get(DataService.NAME);
 
     protected ProcessAction(Card card, String actionName, ActionsFrame frame) {
         super(actionName);
@@ -90,9 +92,20 @@ public class ProcessAction extends AbstractAction {
 
         formManagerParams.put("subProcCard", new CardContext());
 
+        WindowManagerProvider windowManagerProvider = AppBeans.get(WindowManagerProvider.NAME);
+        if (isCardDeleted(card)) {
+            windowManagerProvider.get().showNotification(
+                    messages.getMessage(getClass(), "cardWasDeletedByAnotherUser"),
+                    IFrame.NotificationType.WARNING
+            );
+            window.close(Window.CLOSE_ACTION_ID, true);
+            return;
+        }
+
+
         //we won't commit the editor if user presses no in cancel process confirmation form
         if (WfConstants.ACTION_CANCEL.equals(actionName)) {
-            if (isCardInProcess(card)) {
+            if (isCardInProcess(card) && isCardInSameProcess(card)) {
                 App.getInstance().getWindowManager().showOptionDialog(
                         messages.getMessage(getClass(), "cancelProcess.title"),
                         messages.formatMessage(getClass(), "cancelProcess.message", card.getProc().getName()),
@@ -127,6 +140,11 @@ public class ProcessAction extends AbstractAction {
                                     @Override
                                     public void actionPerform(Component c) {
                                         window.close(Window.CLOSE_ACTION_ID, true);
+                                    }
+                                },
+                                new DialogAction(DialogAction.Type.NO) {
+                                    @Override
+                                    public void actionPerform(Component c) {
                                     }
                                 }
                         }
@@ -241,10 +259,50 @@ public class ProcessAction extends AbstractAction {
         managerChain.doManagerAfter();
     }
 
+    protected boolean isCardDeleted(Card card) {
+        if (PersistenceHelper.isNew(card)) {
+            return false;
+        }
+
+        LoadContext lc = new LoadContext(Card.class).setId(card.getId()).setView(View.LOCAL);
+        Card reloadedCard = dataService.load(lc);
+        return (reloadedCard == null);
+    }
+
+    /**
+     * Notice that next two methods are called in #actionPerform() after method #isCardDeleted()
+     * That's why they can't throw NullPointerException
+     * If you change this logic, handle the exception.
+     */
     protected boolean isCardInProcess(Card card) {
         LoadContext lc = new LoadContext(Card.class).setId(card.getId()).setView(View.LOCAL);
-        DataService dataService = AppBeans.get(DataService.NAME);
         Card reloadedCard = dataService.load(lc);
         return (reloadedCard.getJbpmProcessId() != null);
+    }
+
+    protected boolean isCardInSameProcess(Card card) {
+        Metadata metadata = AppBeans.get(Metadata.NAME);
+        View withProcess = metadata.getViewRepository().getView(Card.class, "w-card-proc");
+        LoadContext lc = new LoadContext(Card.class).setId(card.getId()).setView(withProcess);
+        Card reloadedCard = dataService.load(lc);
+
+        if (card.getProcs() == null) {
+            return true;
+        }
+
+        CardProc cardCP = !card.getProcs().isEmpty() ? card.getProcs().get(0) : null;
+        CardProc reloadedCardCP = null;
+        if (cardCP != null && reloadedCard != null && !reloadedCard.getProcs().isEmpty()) {
+            for (CardProc cp : reloadedCard.getProcs()) {
+                if (cp.getProc() != null && cardCP.getProc() != null &&
+                        cp.getProc().equals(cardCP.getProc())) {
+                    reloadedCardCP = cp;
+                }
+            }
+        }
+
+        return (cardCP != null && reloadedCardCP != null
+                && cardCP.equals(reloadedCardCP)
+                && cardCP.getStartCount().equals(reloadedCardCP.getStartCount()));
     }
 }
