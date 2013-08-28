7b515f80aa55b4d09275c6d7cb4e66fd94ad3ddd
Konstantin Krivopustov <krivopustov@haulmont.com>
2013-08-28 11:12:52 +0000

    Clean up from deprecated code. #PL-2230

---
+++ modules/web/src/com/haulmont/workflow/web/ui/card/CardBrowser.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2013 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+ */
+package com.haulmont.workflow.web.ui.card;
+
+import com.haulmont.cuba.gui.WindowManager;
+import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.actions.CreateAction;
+import com.haulmont.cuba.gui.components.actions.RefreshAction;
+import com.haulmont.cuba.gui.components.actions.RemoveAction;
+import com.haulmont.cuba.web.App;
+import com.haulmont.workflow.core.entity.Card;
+
+import javax.inject.Named;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author krivopustov
+ * @version $Id$
+ */
+public class CardBrowser extends AbstractWindow {
+
+    @Named("cardTable")
+    protected Table table;
+
+    public void init(Map<String, Object> params) {
+        super.init(params);
+        table.addAction(new RefreshAction(table));
+        table.addAction(new CreateAction(table));
+        table.addAction(new AbstractAction("open") {
+            @Override
+            public void actionPerform(Component component) {
+                    Set<Card> selected = table.getSelected();
+                    if (selected.size() == 1) {
+                        Window window = openEditor("wf$Card.edit", selected.iterator().next(), WindowManager.OpenType.THIS_TAB);
+                        window.addListener(new CloseListener() {
+                            @Override
+                            public void windowClosed(String actionId) {
+                                if (Window.COMMIT_ACTION_ID.equals(actionId)) {
+                                    table.getDatasource().refresh();
+                                }
+                            }
+                        });
+                    }
+            }
+        });
+        table.addAction(new RemoveAction(table));
+        table.addAction(new ViewVariablesAction());
+    }
+
+    private class ViewVariablesAction extends AbstractAction {
+
+        public ViewVariablesAction() {
+            super("viewVariables");
+        }
+
+        public void actionPerform(Component component) {
+            Set selected = table.getSelected();
+            if (!selected.isEmpty()) {
+                final Card card = (Card) selected.iterator().next();
+                App.getInstance().getWindowManager().getDialogParams().setWidth(700);
+                openWindow("wf$CardVariable.browse", WindowManager.OpenType.DIALOG, Collections.<String, Object> singletonMap("card", card));
+            }
+        }
+    }
+}
