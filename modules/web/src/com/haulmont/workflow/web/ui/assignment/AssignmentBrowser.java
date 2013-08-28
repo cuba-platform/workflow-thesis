ac9c019d9fcf55554975166b6d5d850e76091ec3
Konstantin Krivopustov <krivopustov@haulmont.com>
2013-08-28 11:12:52 +0000

    Clean up from deprecated code. #PL-2230

---
+++ modules/web/src/com/haulmont/workflow/web/ui/assignment/AssignmentBrowser.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2009 Haulmont Technology Ltd. All Rights Reserved.
+ * Haulmont Technology proprietary and confidential.
+ * Use is subject to license terms.
+ */
+package com.haulmont.workflow.web.ui.assignment;
+
+import com.haulmont.cuba.gui.WindowManager;
+import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.actions.RefreshAction;
+import com.haulmont.workflow.core.entity.Assignment;
+import com.haulmont.workflow.core.entity.Card;
+
+import javax.inject.Named;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author krivopustov
+ * @version $Id$
+ */
+public class AssignmentBrowser extends AbstractWindow {
+
+    @Named("aTable")
+    protected Table table;
+
+    public void init(Map<String, Object> params) {
+        super.init(params);
+        table.addAction(new RefreshAction(table));
+        table.addAction(new AbstractAction("open") {
+            @Override
+            public void actionPerform(Component component) {
+                Set<Assignment> selected = table.getSelected();
+                if (selected.size() == 1) {
+                    Card card = selected.iterator().next().getCard();
+                    Window window = openEditor("wf$Card.edit", card, WindowManager.OpenType.THIS_TAB);
+                    window.addListener(new CloseListener() {
+                        @Override
+                        public void windowClosed(String actionId) {
+                            if (Window.COMMIT_ACTION_ID.equals(actionId)) {
+                                table.getDatasource().refresh();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+}
