9bfb286800fc0440e7f3cf08e6c13384f34acf8b
Sergey Saiyan <saiyan@haulmont.com>
2013-09-23 14:19:30 +0000

    QueryRunner usage in CardAttachmentEntityListener to avoid OptimisticLocking exception workflow_trunk #PL-2692 Fixed

--- modules/core/src/com/haulmont/workflow/core/listeners/CardAttachmentEntityListener.java
+++ modules/core/src/com/haulmont/workflow/core/listeners/CardAttachmentEntityListener.java
@@ -4,32 +4,57 @@
  */
 package com.haulmont.workflow.core.listeners;
 
+import com.haulmont.bali.db.QueryRunner;
+import com.haulmont.cuba.core.Persistence;
 import com.haulmont.cuba.core.global.AppBeans;
-import com.haulmont.cuba.core.global.PersistenceHelper;
+import com.haulmont.cuba.core.listener.AfterInsertEntityListener;
 import com.haulmont.cuba.core.listener.BeforeDeleteEntityListener;
-import com.haulmont.cuba.core.listener.BeforeInsertEntityListener;
-import com.haulmont.workflow.core.app.WfWorkerAPI;
+import com.haulmont.cuba.core.sys.persistence.DbTypeConverter;
 import com.haulmont.workflow.core.entity.Card;
 import com.haulmont.workflow.core.entity.CardAttachment;
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-public class CardAttachmentEntityListener implements BeforeInsertEntityListener<CardAttachment>, BeforeDeleteEntityListener<CardAttachment> {
-    public void onBeforeInsert(CardAttachment entity) {
-        entity.getCard().setHasAttachments(true);
-        if (!PersistenceHelper.isNew(entity.getCard()))
-            setHasAttachmentsInCard(entity.getCard(), true);
+import java.sql.SQLException;
+
+public class CardAttachmentEntityListener implements AfterInsertEntityListener<CardAttachment>, BeforeDeleteEntityListener<CardAttachment> {
+
+    protected Log log = LogFactory.getLog(CardAttachmentEntityListener.class);
+
+    protected Persistence persistence = AppBeans.get(Persistence.NAME);
+
+    protected DbTypeConverter converter = persistence.getDbTypeConverter();
+
+    protected static final String CARD_UPDATE_QUERY = "update WF_CARD set HAS_ATTACHMENTS = ? where ID = ?";
+
+    @Override
+    public void onAfterInsert(CardAttachment entity) {
+        executeUpdate(entity.getCard(), Boolean.TRUE);
     }
 
+    @Override
     public void onBeforeDelete(CardAttachment entity) {
         Card card = entity.getCard();
         card.getAttachments().remove(entity);
         if (card.getAttachments().isEmpty()) {
-            card.setHasAttachments(false);
-            setHasAttachmentsInCard(card, false);
+            executeUpdate(card, Boolean.FALSE);
         }
     }
 
-    private void setHasAttachmentsInCard(Card card, Boolean hasAttachments) {
-        WfWorkerAPI wfWorkerAPI = AppBeans.get(WfWorkerAPI.NAME);
-        wfWorkerAPI.setHasAttachmentsInCard(card, hasAttachments);
+    /**
+     * Use QueryRunner here to avoid OptimisticLocking exception
+     */
+    protected void executeUpdate(Card card, Boolean hasAttachments) {
+        try {
+            Object hasAttachmentsParam = converter.getSqlObject(hasAttachments);
+            Object idParam = converter.getSqlObject(card.getId());
+
+            QueryRunner runner = new QueryRunner();
+            runner.update(persistence.getEntityManager().getConnection(),
+                    CARD_UPDATE_QUERY, new Object[]{hasAttachmentsParam, idParam});
+        } catch (SQLException e) {
+            log.error(ExceptionUtils.getStackTrace(e));
+        }
     }
 }
