c52bf9fc8e2e5be47c451d436f47dc51852e6342
Andrey Tsarevskiy <tsarevskiy@haulmont.com>
2014-01-16 09:18:40 +0000

    Убрать использование executeUpdate в TimerManager.removeTimers #PL-3439 Fixed

--- modules/core/src/com/haulmont/workflow/core/app/TimerManager.java
+++ modules/core/src/com/haulmont/workflow/core/app/TimerManager.java
@@ -91,19 +91,13 @@ public class TimerManager implements TimerManagerAPI {
     public void removeTimers(ActivityExecution execution) {
         checkNotNull(execution, "execution is null");
 
-        Transaction tx = persistence.getTransaction();
-        try {
-            EntityManager em = persistence.getEntityManager();
-            Query q = em.createQuery("select t from wf$Timer t where t.jbpmExecutionId = ?1 and t.activity = ?2");
-            q.setParameter(1, execution.getId());
-            q.setParameter(2, execution.getActivityName());
-            List<TimerEntity> timerEntities = q.getResultList();
-            for (TimerEntity timerEntity : timerEntities) {
-                em.remove(timerEntity);
-            }
-            tx.commit();
-        } finally {
-            tx.end();
+        EntityManager em = persistence.getEntityManager();
+        Query q = em.createQuery("select t from wf$Timer t where t.jbpmExecutionId = ?1 and t.activity = ?2");
+        q.setParameter(1, execution.getId());
+        q.setParameter(2, execution.getActivityName());
+        List<TimerEntity> timerEntities = q.getResultList();
+        for (TimerEntity timerEntity : timerEntities) {
+            em.remove(timerEntity);
         }
     }
 
