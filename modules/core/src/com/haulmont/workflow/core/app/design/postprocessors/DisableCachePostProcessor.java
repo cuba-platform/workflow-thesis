d3c8a29109301901ba7f05fc709bfeb1b7a1cffb
Evgeny Zaharchenko <zaharchenko@haulmont.com>
2014-02-04 12:55:31 +0000

    fix #PL-3376

--- modules/core/src/com/haulmont/workflow/core/app/design/postprocessors/DisableCachePostProcessor.java
+++ modules/core/src/com/haulmont/workflow/core/app/design/postprocessors/DisableCachePostProcessor.java
@@ -20,7 +20,7 @@ public class DisableCachePostProcessor extends BaseDesignPostProcessor {
 
     @Override
     public void processJpdl(Element rootElement, List<DesignCompilationError> compileErrors) {
-        List<Element> activities = (List<Element>) rootElement.elements("custom");
+        List<Element> activities = (List<Element>) rootElement.elements();
         if (!activities.isEmpty()) {
             for (Element element : activities) {
                 element.addAttribute("cache", "disabled");
