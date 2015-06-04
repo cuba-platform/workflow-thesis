0011036f97aa2371472c1f063efb74938eb73893
Evgeny Zaharchenko <zaharchenko@haulmont.com>
2015-06-04 13:04:09 +0000

    refs #PL-5485 add localization to pretty time

--- modules/core/src/com/haulmont/workflow/core/app/valuehandler/DateCardPropertyHandler.java
+++ modules/core/src/com/haulmont/workflow/core/app/valuehandler/DateCardPropertyHandler.java
@@ -5,13 +5,9 @@
 
 package com.haulmont.workflow.core.app.valuehandler;
 
+import com.haulmont.cuba.core.app.prettytime.CubaPrettyTimeParser;
+import com.haulmont.cuba.core.global.AppBeans;
 import com.haulmont.workflow.core.entity.Card;
-import org.ocpsoft.prettytime.nlp.PrettyTimeParser;
-
-import java.util.Date;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author zaharchenko
@@ -19,39 +15,12 @@ import java.util.Map;
  */
 public class DateCardPropertyHandler extends BaseCardPropertyHandler {
 
-    private static Map<String, String> REPLACEMENT = new LinkedHashMap<String, String>() {
-        {
-            put("current time", "now");
-            put("current date", "now");
-            put("current", "now");
-            put("+", "plus");
-            put("-", "minus");
-            put("previous", "last");
-        }
-    };
-
     public DateCardPropertyHandler(Class clazz, Card card, Boolean useExpression) {
         super(clazz, card, useExpression);
     }
 
     protected Object evaluateExpression(String value) {
-
-        String formattedValue = " " + value.toLowerCase().replaceAll("\\s+", " ") + " ";
-        for (String key : REPLACEMENT.keySet()) {
-            formattedValue = formattedValue.replace(" " + key + " ", " " + REPLACEMENT.get(key) + " ");
-        }
-        formattedValue = formattedValue.trim();
-        List<Date> dates = new PrettyTimeParser().parse(formattedValue);
-        if (dates.isEmpty()) {
-            return null;
-        }
-        if (formattedValue.contains(" minus ") && dates.size() == 2) {
-            Long time1 = dates.get(0).getTime();
-            Long time2 = dates.get(1).getTime();
-            Long difference = time2 - time1;
-            Long resultTime = time1 - difference;
-            return new Date(resultTime);
-        }
-        return dates.get(dates.size() - 1);
+        CubaPrettyTimeParser prettyTimeParser = AppBeans.get(CubaPrettyTimeParser.NAME);
+        return prettyTimeParser.parse(value);
     }
 }
