816fec49b0e8c24298e14df4ba4bc5cdeb7367b2
Konstantin Krivopustov <krivopustov@haulmont.com>
2015-07-15 11:49:10 +0000

    Workflow - update scripts for Oracle and HSQL. #PL-5647

--- modules/core/db/init/hsql/create-db.sql
+++ modules/core/db/init/hsql/create-db.sql
@@ -143,6 +143,17 @@ alter table WF_CARD_COMMENT add constraint FK_WF_CARD_COMMENT_SUB_USER foreign k
 
 ------------------------------------------------------------------------------------------------------------
 
+create table WF_CARD_COMMENT_USER (
+    CARD_COMMENT_ID varchar(36),
+    USER_ID varchar(36),
+    primary key (CARD_COMMENT_ID, USER_ID)
+)^
+
+alter table WF_CARD_COMMENT_USER add constraint FK_WF_CCU_CARD_COMMENT foreign key (CARD_COMMENT_ID) references WF_CARD_COMMENT (ID)^
+alter table WF_CARD_COMMENT_USER add constraint FK_WF_CCU_USER foreign key (USER_ID) references SEC_USER (ID)^
+
+------------------------------------------------------------------------------------------------------------
+
 create table WF_CARD_RELATION (
     ID varchar(36) not null,
     CREATE_TS timestamp,
