d7afa70ee41fabdf67cc9c1f4c4c831131d20b9e
Andrey Mishunin <mishunin@haulmont.com>
2015-10-12 13:12:42 +0000

    Убрать модуль Цикл из дизайнера процессов #PL-6150

--- modules/web/web/wfdesigner/common/lang.js
+++ modules/web/web/wfdesigner/common/lang.js
@@ -409,43 +409,6 @@ var wfLanguage = {
             }
         },
         {
-            "name" : "ForEachUser",
-            "label" : "msg://ForEachUser",
-            "category" : "common",
-            "container" : {
-                "xtype":"Wf.ImageContainer",
-                "className": "WireIt-Container WireIt-ImageContainer Wf-Fork",
-                "icon": "../common/res/icons/fork_icon.png",
-                "image": "../common/res/icons/fork.png",
-                "selectedImage": "../common/res/icons/fork-selected.png",
-                "optFields": [
-                    {
-                        "type": "string",
-                        "label": "msg://name",
-                        "name": "name",
-                        "value": "msg://ForEachUser"
-                    },
-                    {
-                        "type": "string",
-                        "name": "role",
-                        "label": "msg://role",
-                        "required": true,
-                        "value": ""
-                    }
-                ],
-                "terminals": [
-                    {
-                        "direction": [0,-1], "offsetPosition": {"left": 8, "top": -15 }, "name": "in",
-                        "ddConfig": {"type": "in","allowedTypes": ["out"]}
-                    },
-                    {
-                        "direction": [0,1], "offsetPosition": {"left": 8, "bottom": -15}, "name": "out",
-                        "ddConfig": {"type": "out","allowedTypes": ["in"]}, "alwaysSrc": true, "nMaxWires": 1
-                    }
-                ]
-            }
-        },
-        {
             "name" : "Join",
             "label" : "msg://Join",
             "category" : "common",
