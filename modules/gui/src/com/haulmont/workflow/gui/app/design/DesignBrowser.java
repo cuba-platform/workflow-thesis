a0ba4d68b585d91a2233224e0b4bd091a26a001b
Andrey Mishunin <mishunin@haulmont.com>
2015-10-21 11:39:34 +0000

    Revert: Create DesignBrowser.CompileAction#afterCompile method #PL-6222

--- modules/gui/src/com/haulmont/workflow/gui/app/design/DesignBrowser.java
+++ modules/gui/src/com/haulmont/workflow/gui/app/design/DesignBrowser.java
@@ -354,8 +354,6 @@ public class DesignBrowser extends AbstractWindow {
                 }
                 result.append("<br>");
             }
-            String afterCompileResult = afterCompile();
-            result.append(afterCompileResult);
             showMessageDialog(getMessage("compilationResult"), result.toString(), MessageType.CONFIRMATION_HTML);
         }
 
@@ -375,10 +373,6 @@ public class DesignBrowser extends AbstractWindow {
             } catch (DesignCompilationException e) {
                 return new Pair<>(getMessage("notification.compileFailed"), e.getMessage());
             }
-        }
-
-        protected String afterCompile() {
-            return "";
         }
 
         private String prepareCompilationMessage(CompilationMessage message) {
