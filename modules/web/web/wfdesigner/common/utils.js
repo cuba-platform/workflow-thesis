85f8afbf0875b2c755ac49b4c68e9a958ba8a6bf
Andrey Mishunin <mishunin@haulmont.com>
2015-11-10 12:29:47 +0000

    Спецсимволы отображаются некорректно в блоках "Проверить значение карточки" и "Записать значение карточки" #PL-6291

--- modules/web/web/wfdesigner/common/utils.js
+++ modules/web/web/wfdesigner/common/utils.js
@@ -142,10 +142,9 @@ Wf = {
     },
 
     parseHtmlEntities: function(str) {
-        return str.replace(/&#([0-9]{1,4});/gi, function(match, numStr) {
-            var num = parseInt(numStr, 10);
-            return String.fromCharCode(num);
-        });
+        var decoder = document.createElement('textarea');
+        decoder.innerHTML = str;
+        return decoder.value;
     }
 };
 
