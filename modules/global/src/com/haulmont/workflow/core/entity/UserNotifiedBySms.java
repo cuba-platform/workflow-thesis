c5366ce96fadffdc0bab698afc92b626ee149413
Konstantin Krivopustov <krivopustov@haulmont.com>
2017-10-04 12:31:41 +0400

    PL-8269 Workflow UserNotifiedBySms does not have createTs and createdBy

--- modules/global/src/com/haulmont/workflow/core/entity/UserNotifiedBySms.java
+++ modules/global/src/com/haulmont/workflow/core/entity/UserNotifiedBySms.java
@@ -6,22 +6,47 @@
 package com.haulmont.workflow.core.entity;
 
 import com.haulmont.cuba.core.entity.BaseUuidEntity;
+import com.haulmont.cuba.core.entity.Creatable;
 import com.haulmont.cuba.security.entity.User;
 
 import javax.persistence.*;
+import java.util.Date;
 
-/**
- *
- */
 @Entity(name = "wf$UserNotifiedBySms")
 @Table(name = "WF_USER_NOTIFIED_BY_SMS")
-public class UserNotifiedBySms extends BaseUuidEntity {
+public class UserNotifiedBySms extends BaseUuidEntity implements Creatable {
 
     private static final long serialVersionUID = -4233245010821767444L;
+
+    @Column(name = "CREATE_TS")
+    protected Date createTs;
+
+    @Column(name = "CREATED_BY", length = 50)
+    protected String createdBy;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID")
     protected User user;
+
+    @Override
+    public Date getCreateTs() {
+        return createTs;
+    }
+
+    @Override
+    public void setCreateTs(Date createTs) {
+        this.createTs = createTs;
+    }
+
+    @Override
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    @Override
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
 
     public User getUser() {
         return user;
