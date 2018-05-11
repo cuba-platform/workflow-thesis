5f28f5deb84995791a1063cacedd65df2ce19ecc
Ilya Praht <praht@haulmont.com>
2018-05-11 16:25:57 +0400

    #PL-10751 fixed oracle scripts char semantics

--- modules/core/db/init/oracle/jbpm.create-db.sql
+++ modules/core/db/init/oracle/jbpm.create-db.sql
@@ -2,16 +2,16 @@ create table JBPM4_DEPLOYMENT (
     DBID_ number not null,
     NAME_ clob,
     TIMESTAMP_ number,
-    STATE_ varchar2(255),
+    STATE_ varchar2(255 char),
     primary key(DBID_)
 )^
 
 create table JBPM4_DEPLOYPROP (
     DBID_ number not null,
     DEPLOYMENT_ number,
-    OBJNAME_ varchar2(255),
-    KEY_ varchar2(255),
-    STRINGVAL_ varchar2(255),
+    OBJNAME_ varchar2(255 char),
+    KEY_ varchar2(255 char),
+    STRINGVAL_ varchar2(255 char),
     LONGVAL_ number,
     primary key(DBID_)
 )^
@@ -19,16 +19,16 @@ create index IDX_DEPLPROP_DEPL on JBPM4_DEPLOYPROP(DEPLOYMENT_)^
 
 create table JBPM4_EXECUTION (
     DBID_ number not null,
-    CLASS_ varchar2(255) not null,
+    CLASS_ varchar2(255 char) not null,
     DBVERSION_ integer not null,
-    ACTIVITYNAME_ varchar2(255),
-    PROCDEFID_ varchar2(255),
+    ACTIVITYNAME_ varchar2(255 char),
+    PROCDEFID_ varchar2(255 char),
     HASVARS_ char(1),
-    NAME_ varchar2(255),
-    KEY_ varchar2(255),
-    ID_ varchar2(255),
-    STATE_ varchar2(255),
-    SUSPHISTSTATE_ varchar2(255),
+    NAME_ varchar2(255 char),
+    KEY_ varchar2(255 char),
+    ID_ varchar2(255 char),
+    STATE_ varchar2(255 char),
+    SUSPHISTSTATE_ varchar2(255 char),
     PRIORITY_ integer,
     HISACTINST_ number,
     PARENT_ number,
@@ -46,16 +46,16 @@ create index IDX_EXEC_SUPEREXEC on JBPM4_EXECUTION(SUPEREXEC_)^
 
 create table JBPM4_HIST_ACTINST (
     DBID_ number not null,
-    CLASS_ varchar2(255) not null,
+    CLASS_ varchar2(255 char) not null,
     DBVERSION_ integer not null,
     HPROCI_ number,
-    TYPE_ varchar2(255),
-    EXECUTION_ varchar2(255),
-    ACTIVITY_NAME_ varchar2(255),
+    TYPE_ varchar2(255 char),
+    EXECUTION_ varchar2(255 char),
+    ACTIVITY_NAME_ varchar2(255 char),
     START_ timestamp,
     END_ timestamp,
     DURATION_ number,
-    TRANSITION_ varchar2(255),
+    TRANSITION_ varchar2(255 char),
     NEXTIDX_ integer,
     HTASK_ number,
     primary key(DBID_)
@@ -65,9 +65,9 @@ create index IDX_HTI_HTASK on JBPM4_HIST_ACTINST(HTASK_)^
 
 create table JBPM4_HIST_DETAIL (
     DBID_ number not null,
-    CLASS_ varchar2(255) not null,
+    CLASS_ varchar2(255 char) not null,
     DBVERSION_ integer not null,
-    USERID_ varchar2(255),
+    USERID_ varchar2(255 char),
     TIME_ timestamp,
     HPROCI_ number,
     HPROCIIDX_ integer,
@@ -78,8 +78,8 @@ create table JBPM4_HIST_DETAIL (
     HVAR_ number,
     HVARIDX_ integer,
     MESSAGE_ clob,
-    OLD_STR_ varchar2(255),
-    NEW_STR_ varchar2(255),
+    OLD_STR_ varchar2(255 char),
+    NEW_STR_ varchar2(255 char),
     OLD_INT_ integer,
     NEW_INT_ integer,
     OLD_TIME_ timestamp,
@@ -96,14 +96,14 @@ create index IDX_HDET_HVAR on JBPM4_HIST_DETAIL(HVAR_)^
 create table JBPM4_HIST_PROCINST (
     DBID_ number not null,
     DBVERSION_ integer not null,
-    ID_ varchar2(255),
-    PROCDEFID_ varchar2(255),
-    KEY_ varchar2(255),
+    ID_ varchar2(255 char),
+    PROCDEFID_ varchar2(255 char),
+    KEY_ varchar2(255 char),
     START_ timestamp,
     END_ timestamp,
     DURATION_ number,
-    STATE_ varchar2(255),
-    ENDACTIVITY_ varchar2(255),
+    STATE_ varchar2(255 char),
+    ENDACTIVITY_ varchar2(255 char),
     NEXTIDX_ integer,
     primary key(DBID_)
 )^
@@ -111,11 +111,11 @@ create table JBPM4_HIST_PROCINST (
 create table JBPM4_HIST_TASK (
     DBID_ number not null,
     DBVERSION_ integer not null,
-    EXECUTION_ varchar2(255),
-    OUTCOME_ varchar2(255),
-    ASSIGNEE_ varchar2(255),
+    EXECUTION_ varchar2(255 char),
+    OUTCOME_ varchar2(255 char),
+    ASSIGNEE_ varchar2(255 char),
     PRIORITY_ integer,
-    STATE_ varchar2(255),
+    STATE_ varchar2(255 char),
     CREATE_ timestamp,
     END_ timestamp,
     DURATION_ number,
@@ -128,10 +128,10 @@ create index IDX_HSUPERT_SUB on JBPM4_HIST_TASK(SUPERTASK_)^
 create table JBPM4_HIST_VAR (
     DBID_ number not null,
     DBVERSION_ integer not null,
-    PROCINSTID_ varchar2(255),
-    EXECUTIONID_ varchar2(255),
-    VARNAME_ varchar2(255),
-    VALUE_ varchar2(255),
+    PROCINSTID_ varchar2(255 char),
+    EXECUTIONID_ varchar2(255 char),
+    VARNAME_ varchar2(255 char),
+    VALUE_ varchar2(255 char),
     HPROCI_ number,
     HTASK_ number,
     primary key(DBID_)
@@ -142,9 +142,9 @@ create index IDX_HVAR_HTASK on JBPM4_HIST_VAR(HTASK_)^
 create table JBPM4_ID_GROUP (
     DBID_ number not null,
     DBVERSION_ integer not null,
-    ID_ varchar2(255),
-    NAME_ varchar2(255),
-    TYPE_ varchar2(255),
+    ID_ varchar2(255 char),
+    NAME_ varchar2(255 char),
+    TYPE_ varchar2(255 char),
     PARENT_ number,
     primary key(DBID_)
 )^
@@ -155,7 +155,7 @@ create table JBPM4_ID_MEMBERSHIP (
     DBVERSION_ integer not null,
     USER_ number,
     GROUP_ number,
-    NAME_ varchar2(255),
+    NAME_ varchar2(255 char),
     primary key(DBID_)
 )^
 create index IDX_MEM_GROUP on JBPM4_ID_MEMBERSHIP(GROUP_)^
@@ -164,31 +164,31 @@ create index IDX_MEM_USER on JBPM4_ID_MEMBERSHIP(USER_)^
 create table JBPM4_ID_USER (
     DBID_ number not null,
     DBVERSION_ integer not null,
-    ID_ varchar2(255),
-    PASSWORD_ varchar2(255),
-    GIVENNAME_ varchar2(255),
-    FAMILYNAME_ varchar2(255),
-    BUSINESSEMAIL_ varchar2(255),
+    ID_ varchar2(255 char),
+    PASSWORD_ varchar2(255 char),
+    GIVENNAME_ varchar2(255 char),
+    FAMILYNAME_ varchar2(255 char),
+    BUSINESSEMAIL_ varchar2(255 char),
     primary key(DBID_)
 )^
 
 create table JBPM4_JOB (
     DBID_ number not null,
-    CLASS_ varchar2(255) not null,
+    CLASS_ varchar2(255 char) not null,
     DBVERSION_ integer not null,
     DUEDATE_ timestamp,
-    STATE_ varchar2(255),
+    STATE_ varchar2(255 char),
     ISEXCLUSIVE_ char(1),
-    LOCKOWNER_ varchar2(255),
+    LOCKOWNER_ varchar2(255 char),
     LOCKEXPTIME_ timestamp,
     EXCEPTION_ clob,
     RETRIES_ integer,
     PROCESSINSTANCE_ number,
     EXECUTION_ number,
     CFG_ number,
-    SIGNAL_ varchar2(255),
-    EVENT_ varchar2(255),
-    REPEAT_ varchar2(255),
+    SIGNAL_ varchar2(255 char),
+    EVENT_ varchar2(255 char),
+    REPEAT_ varchar2(255 char),
     primary key(DBID_)
 )^
 create index IDX_JOB_CFG on JBPM4_JOB(CFG_)^
@@ -211,9 +211,9 @@ create index IDX_LOB_DEPLOYMENT on JBPM4_LOB(DEPLOYMENT_)^
 create table JBPM4_PARTICIPATION (
     DBID_ number not null,
     DBVERSION_ integer not null,
-    GROUPID_ varchar2(255),
-    USERID_ varchar2(255),
-    TYPE_ varchar2(255),
+    GROUPID_ varchar2(255 char),
+    USERID_ varchar2(255 char),
+    TYPE_ varchar2(255 char),
     TASK_ number,
     SWIMLANE_ number,
     primary key(DBID_)
@@ -221,17 +221,17 @@ create table JBPM4_PARTICIPATION (
 create index IDX_PART_TASK on JBPM4_PARTICIPATION(TASK_)^
 
 create table JBPM4_PROPERTY (
-    KEY_ varchar2(255) not null,
+    KEY_ varchar2(255 char) not null,
     VERSION_ integer not null,
-    VALUE_ varchar2(255),
+    VALUE_ varchar2(255 char),
     primary key(KEY_)
 )^
 
 create table JBPM4_SWIMLANE (
     DBID_ number not null,
     DBVERSION_ integer not null,
-    NAME_ varchar2(255),
-    ASSIGNEE_ varchar2(255),
+    NAME_ varchar2(255 char),
+    ASSIGNEE_ varchar2(255 char),
     EXECUTION_ number,
     primary key(DBID_)
 )^
@@ -241,42 +241,42 @@ create table JBPM4_TASK (
     DBID_ number not null,
     CLASS_ char(1) not null,
     DBVERSION_ integer not null,
-    NAME_ varchar2(255),
+    NAME_ varchar2(255 char),
     DESCR_ clob,
-    STATE_ varchar2(255),
-    SUSPHISTSTATE_ varchar2(255),
-    ASSIGNEE_ varchar2(255),
-    FORM_ varchar2(255),
+    STATE_ varchar2(255 char),
+    SUSPHISTSTATE_ varchar2(255 char),
+    ASSIGNEE_ varchar2(255 char),
+    FORM_ varchar2(255 char),
     PRIORITY_ integer,
     CREATE_ timestamp,
     DUEDATE_ timestamp,
     PROGRESS_ integer,
     SIGNALLING_ char(1),
-    EXECUTION_ID_ varchar2(255),
-    ACTIVITY_NAME_ varchar2(255),
+    EXECUTION_ID_ varchar2(255 char),
+    ACTIVITY_NAME_ varchar2(255 char),
     HASVARS_ char(1),
     SUPERTASK_ number,
     EXECUTION_ number,
     PROCINST_ number,
     SWIMLANE_ number,
-    TASKDEFNAME_ varchar2(255),
+    TASKDEFNAME_ varchar2(255 char),
     primary key(DBID_)
 )^
 create index IDX_TASK_SUPERTASK on JBPM4_TASK(SUPERTASK_)^
 
 create table JBPM4_VARIABLE (
     DBID_ number not null,
-    CLASS_ varchar2(255) not null,
+    CLASS_ varchar2(255 char) not null,
     DBVERSION_ integer not null,
-    KEY_ varchar2(255),
-    CONVERTER_ varchar2(255),
+    KEY_ varchar2(255 char),
+    CONVERTER_ varchar2(255 char),
     HIST_ char(1),
     EXECUTION_ number,
     TASK_ number,
     LOB_ number,
     DATE_VALUE_ timestamp,
     DOUBLE_VALUE_ float,
-    CLASSNAME_ varchar2(255),
+    CLASSNAME_ varchar2(255 char),
     LONG_VALUE_ number,
     STRING_VALUE_ clob,
     TEXT_VALUE_ clob,
