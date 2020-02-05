a4f99efe3f964ea07f35712dd697cfc2f47e355f
Sergey Karasev <karasev@haulmont.com>
2020-02-05 18:21:37 +0400

    PL-10791 Refactor com.haulmont.workflow.gui.app.base.ResolutionForm

--- modules/gui/src/com/haulmont/workflow/gui/app/base/ResolutionForm.java
+++ modules/gui/src/com/haulmont/workflow/gui/app/base/ResolutionForm.java
@@ -127,14 +127,7 @@ public class ResolutionForm extends AbstractForm {
         copyAttachBtn.setCaption(messages.getMessage(getClass(), "actions.Copy"));
 
         Button pasteAttachBtn = getComponent("pasteAttach");
-        AttachmentCreator creator = new AttachmentCreator() {
-            public Attachment createObject() {
-                CardAttachment attachment = metadata.create(CardAttachment.class);
-                attachment.setAssignment(assignmentDs.getItem());
-                attachment.setCard(assignmentDs.getItem().getCard());
-                return attachment;
-            }
-        };
+        AttachmentCreator creator = createAttachmentCreator();
         pasteAttachBtn.setAction(
                 attachmentActionTools.createPasteAction(attachmentsTable, creator, getPasteActionParams()));
         pasteAttachBtn.setCaption(messages.getMessage(getClass(), "actions.Paste"));
@@ -267,5 +260,20 @@ public class ResolutionForm extends AbstractForm {
         commitContext.getCommitInstances().addAll(processAttachments.copyAttachments(assignmentDs.getItems()));
         getDsContext().getDataSupplier().commit(commitContext);
         getDsContext().commit();
+    }
+
+    protected AttachmentCreator createAttachmentCreator() {
+        return new ResolutionAttachmentCreator();
+    }
+
+    public class ResolutionAttachmentCreator implements AttachmentCreator {
+
+        @Override
+        public Attachment createObject() {
+            CardAttachment attachment = metadata.create(CardAttachment.class);
+            attachment.setAssignment(assignmentDs.getItem());
+            attachment.setCard(assignmentDs.getItem().getCard());
+            return attachment;
+        }
     }
 }
