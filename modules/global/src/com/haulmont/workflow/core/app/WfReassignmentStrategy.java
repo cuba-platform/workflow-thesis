33a3fa0018ea779fa5b5596eff899bbc9a157010
Mikhail Stefanov <stefanov@haulmont.com>
2020-03-24 10:49:12 +0400

    Revert "#TS-13452 При переназначении пользователя на текущий этап не создается назначение если добавленный пользователь уже был среди согласующих на предыдущем этапе"
    This reverts commit d6dcb3d6

--- modules/global/src/com/haulmont/workflow/core/app/WfReassignmentStrategy.java
+++ modules/global/src/com/haulmont/workflow/core/app/WfReassignmentStrategy.java
@@ -3,7 +3,9 @@
  * Use is subject to license terms, see http://www.cuba-platform.com/license for details.
  */
 
-package com.haulmont.workflow.core.app.reassignment;
+package com.haulmont.workflow.core.app;
+
+import com.haulmont.workflow.core.app.reassignment.WfReassignmentPredicatesFactory;
 
 /**
  * @author stefanov
